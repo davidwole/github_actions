a7cbdc9a91b1f082a5c55ed2e2e08286e8fb0069
davidwole <davidwole20@gmail.com>
2026-06-25 13:54:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('App is working')
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
-});
+  console.log(`App is running on http://localhost:${PORT}`)
+})
