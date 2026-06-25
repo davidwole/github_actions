6d3c90379e343f4a282112701a7ddd41f0c506c9
davidwole <davidwole20@gmail.com>
2026-06-25 13:19:17 +0100

    first push

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("App is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
